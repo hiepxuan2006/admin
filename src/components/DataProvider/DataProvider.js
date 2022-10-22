3f2c8967e06623f9f489293ed57c732a0d9c5e54
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-10-22 13:13:39 +0700

    [update]

--- src/components/DataProvider/DataProvider.js
+++ src/components/DataProvider/DataProvider.js
@@ -5,35 +5,34 @@ import * as httpRequest from 'utils/httpRequest';
 
 export const DataContext = React.createContext();
 export const DataProvider = (props) => {
-   const [isLogin, setIsLogin] = React.useState(false);
-   const [loading, setLoading] = React.useState(false);
-   const [user, setUser] = React.useState();
-   const checkLogin = async () => {
-      if (localStorage[TOKEN_NAME]) {
-         httpRequest.setAuthToken(localStorage[TOKEN_NAME]);
-      }
-      try {
-         setLoading(true);
-         const results = await acountService.checkLogin();
-         if (results.success) {
-            // localStorage.setItem('user', results.data);
-            setIsLogin(true);
-            setUser(results.data);
-         }
-         setLoading(false);
-      } catch (error) {
-         setIsLogin(false);
-         setLoading(false);
-      }
-   };
-   React.useEffect(() => {
-      checkLogin();
-   }, []);
+    const [isLogin, setIsLogin] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+    const [user, setUser] = React.useState();
+    const checkLogin = async () => {
+        if (localStorage[TOKEN_NAME]) {
+            httpRequest.setAuthToken(localStorage[TOKEN_NAME]);
+        }
+        try {
+            setLoading(true);
+            const results = await acountService.checkLogin();
+            if (results.success) {
+                setIsLogin(true);
+                setUser(results.data.name);
+            }
+            setLoading(false);
+        } catch (error) {
+            setIsLogin(false);
+            setLoading(false);
+        }
+    };
+    React.useEffect(() => {
+        checkLogin();
+    }, []);
 
-   const value = { isLogin, loading, setIsLogin, user };
-   return (
-      <DataContext.Provider value={value}>
-         {props.children}
-      </DataContext.Provider>
-   );
+    const value = { isLogin, loading, setIsLogin, user, setUser };
+    return (
+        <DataContext.Provider value={value}>
+            {props.children}
+        </DataContext.Provider>
+    );
 };
--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,128 +1,136 @@
 import { useContext } from 'react';
 import { Button, Container, Dropdown, Nav, Navbar } from 'react-bootstrap';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Redirect, useHistory, useLocation } from 'react-router-dom';
 
 import { DataContext } from 'components/DataProvider';
 import routes from 'routes.js';
+import { TOKEN_NAME } from 'utils/Contants';
 
 function Header() {
-   const location = useLocation();
-   const { isLogin, user } = useContext(DataContext);
+    const location = useLocation();
+    const { isLogin, user } = useContext(DataContext);
+    const history = useHistory();
+    const mobileSidebarToggle = (e) => {
+        e.preventDefault();
+        document.documentElement.classList.toggle('nav-open');
+        var node = document.createElement('div');
+        node.id = 'bodyClick';
+        node.onclick = function () {
+            this.parentElement.removeChild(this);
+            document.documentElement.classList.toggle('nav-open');
+        };
+        document.body.appendChild(node);
+    };
 
-   const mobileSidebarToggle = (e) => {
-      e.preventDefault();
-      document.documentElement.classList.toggle('nav-open');
-      var node = document.createElement('div');
-      node.id = 'bodyClick';
-      node.onclick = function () {
-         this.parentElement.removeChild(this);
-         document.documentElement.classList.toggle('nav-open');
-      };
-      document.body.appendChild(node);
-   };
+    const getBrandText = () => {
+        for (let i = 0; i < routes.length; i++) {
+            if (
+                location.pathname.indexOf(routes[i].layout + routes[i].path) !==
+                -1
+            ) {
+                return routes[i].name;
+            }
+        }
+        return 'Brand';
+    };
 
-   const getBrandText = () => {
-      for (let i = 0; i < routes.length; i++) {
-         if (
-            location.pathname.indexOf(routes[i].layout + routes[i].path) !== -1
-         ) {
-            return routes[i].name;
-         }
-      }
-      return 'Brand';
-   };
-   return (
-      <Navbar className="position-sticky fixed-top" bg="light" expand="lg">
-         <Container fluid>
-            <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
-               <Button
-                  variant="dark"
-                  className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
-                  onClick={mobileSidebarToggle}
-               >
-                  <i className="fas fa-ellipsis-v"></i>
-               </Button>
-               <Navbar.Brand
-                  href="#home"
-                  onClick={(e) => e.preventDefault()}
-                  className="mr-2"
-               >
-                  {getBrandText()}
-               </Navbar.Brand>
-            </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
-               <span className="navbar-toggler-bar burger-lines"></span>
-               <span className="navbar-toggler-bar burger-lines"></span>
-               <span className="navbar-toggler-bar burger-lines"></span>
-            </Navbar.Toggle>
-            <Navbar.Collapse id="basic-navbar-nav">
-               <Nav className="nav mr-auto" navbar>
-                  <Nav.Item>
-                     <Nav.Link
-                        data-toggle="dropdown"
-                        href="#pablo"
+    const logout = () => {
+        localStorage.removeItem(TOKEN_NAME);
+        return <Redirect to="/login" />;
+    };
+    return (
+        <Navbar className="position-sticky fixed-top" bg="light" expand="lg">
+            <Container fluid>
+                <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
+                    <Button
+                        variant="dark"
+                        className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
+                        onClick={mobileSidebarToggle}
+                    >
+                        <i className="fas fa-ellipsis-v"></i>
+                    </Button>
+                    <Navbar.Brand
+                        href="#home"
                         onClick={(e) => e.preventDefault()}
-                        className="m-0"
-                     >
-                        <i className="nc-icon nc-palette"></i>
-                        <span className="d-lg-none ml-1">Dashboard</span>
-                     </Nav.Link>
-                  </Nav.Item>
-               </Nav>
-               <Nav className="ml-auto" navbar>
-                  <Dropdown as={Nav.Item}>
-                     <Dropdown.Toggle
-                        aria-expanded={false}
-                        aria-haspopup={true}
-                        as={Nav.Link}
-                        data-toggle="dropdown"
-                        id="navbarDropdownMenuLink"
-                        variant="default"
-                        className="m-0"
-                     >
-                        <span className="no-icon">
-                           {isLogin && user ? user.name : 'Tài khoản'}
-                        </span>
-                     </Dropdown.Toggle>
-                     {isLogin && user ? (
-                        <Dropdown.Menu aria-labelledby="navbarDropdownMenuLink">
-                           <Dropdown.Item
-                              href="#pablo"
-                              onClick={(e) => e.preventDefault()}
-                           >
-                              {user.name}
-                           </Dropdown.Item>
-                           <div className="divider"></div>
-                           <Dropdown.Item
-                              href="#pablo"
-                              onClick={(e) => e.preventDefault()}
-                           >
-                              Đăng Xuất
-                           </Dropdown.Item>
-                        </Dropdown.Menu>
-                     ) : (
-                        <Dropdown.Menu aria-labelledby="navbarDropdownMenuLink">
-                           <Dropdown.Item
-                              href="#pablo"
-                              onClick={(e) => e.preventDefault()}
-                           >
-                              Action
-                           </Dropdown.Item>
-                           <div className="divider"></div>
-                           <Dropdown.Item
-                              href="#pablo"
-                              onClick={(e) => e.preventDefault()}
-                           >
-                              <Link to="/login">Đăng nhập</Link>
-                           </Dropdown.Item>
-                        </Dropdown.Menu>
-                     )}
-                  </Dropdown>
-               </Nav>
-            </Navbar.Collapse>
-         </Container>
-      </Navbar>
-   );
+                        className="mr-2"
+                    >
+                        {getBrandText()}
+                    </Navbar.Brand>
+                </div>
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    className="mr-2"
+                >
+                    <span className="navbar-toggler-bar burger-lines"></span>
+                    <span className="navbar-toggler-bar burger-lines"></span>
+                    <span className="navbar-toggler-bar burger-lines"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="nav mr-auto" navbar>
+                        <Nav.Item>
+                            <Nav.Link
+                                data-toggle="dropdown"
+                                href="#pablo"
+                                onClick={(e) => e.preventDefault()}
+                                className="m-0"
+                            >
+                                <i className="nc-icon nc-palette"></i>
+                                <span className="d-lg-none ml-1">
+                                    Dashboard
+                                </span>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Nav className="mr-5" navbar>
+                        <Dropdown as={Nav.Item}>
+                            <Dropdown.Toggle
+                                aria-expanded={false}
+                                aria-haspopup={true}
+                                as={Nav.Link}
+                                data-toggle="dropdown"
+                                id="navbarDropdownMenuLink"
+                                variant="default"
+                                className="m-0"
+                            >
+                                <span className="no-icon">
+                                    {isLogin && user ? user : 'Tài khoản'}
+                                </span>
+                            </Dropdown.Toggle>
+                            {isLogin && user ? (
+                                <Dropdown.Menu aria-labelledby="navbarDropdownMenuLink">
+                                    <Dropdown.Item
+                                        onClick={(e) => e.preventDefault()}
+                                    >
+                                        {user}
+                                    </Dropdown.Item>
+                                    <div className="divider"></div>
+                                    <Dropdown.Item onClick={(e) => logout()}>
+                                        Đăng Xuất
+                                    </Dropdown.Item>
+                                </Dropdown.Menu>
+                            ) : (
+                                <Dropdown.Menu aria-labelledby="navbarDropdownMenuLink">
+                                    <Dropdown.Item
+                                        href="#pablo"
+                                        onClick={(e) => e.preventDefault()}
+                                    >
+                                        Action
+                                    </Dropdown.Item>
+                                    <div className="divider"></div>
+                                    <Dropdown.Item
+                                        href="#pablo"
+                                        onClick={(e) => e.preventDefault()}
+                                    >
+                                        <Link to="/login">Đăng nhập</Link>
+                                    </Dropdown.Item>
+                                </Dropdown.Menu>
+                            )}
+                        </Dropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Header;
